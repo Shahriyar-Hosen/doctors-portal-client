bc7f87789f0bf9585cd8d3c366d847c12b903080
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-10 23:42:52 +0600

    tabIndex

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
     <div className="navbar bg-base-100 lg:px-12">
       <div className="navbar-start ">
         <div className="dropdown">
-          <label tabindex="0" className="btn btn-ghost lg:hidden">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -45,7 +45,7 @@ const Navbar = () => {
             </svg>
           </label>
           <ul
-            tabindex="0"
+            tabIndex="0"
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             {menuItems}
