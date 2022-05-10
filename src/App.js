88b9be12d7265f6fcbc0b9f69827bd16d41a538d
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-10 13:48:55 +0600

    coustom theame color add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Doctors Portal</h1>
+      <button className="btn btn-primary">Button</button>
     </div>
   );
 }
