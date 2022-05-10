adfff77d5e40756a7769a033ce437ab17fe40e92
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-10 15:52:42 +0600

    navbar full setup done

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -2,9 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const menuItems = (
+    <>
+      <li>
+        <Link to="/home">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/appointment">Appointment</Link>
+      </li>
+      <li>
+        <Link to="/reviews">Reviews</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact Us</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </>
+  );
   return (
     <div class="navbar bg-base-100">
-      <div class="navbar-start">
+      <div class="navbar-start ml-20">
         <div class="dropdown">
           <label tabindex="0" class="btn btn-ghost lg:hidden">
             <svg
@@ -26,88 +48,15 @@ const Navbar = () => {
             tabindex="0"
             class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            {/* <li tabindex="0">
-              <Link to="/" class="justify-between">
-                Parent
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </Link>
-              <ul class="p-2">
-                <li>
-                  <Link to="/home">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-              </ul>
-            </li> */}
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/appointment">Appointment</Link>
-            </li>
-            <li>
-              <Link to="/reviews">Reviews</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact Us</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
+            {menuItems}
           </ul>
         </div>
         <Link to="/" class="btn btn-ghost normal-case text-xl">
           Doctors Portal
         </Link>
       </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal p-0">
-          <li>
-            <Link to="/">Item 1</Link>
-          </li>
-          <li tabindex="0">
-            <Link to="/">
-              Parent
-              <svg
-                class="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </Link>
-            <ul class="p-2">
-              <li>
-                <Link to="/">Submenu 1</Link>
-              </li>
-              <li>
-                <Link to="/">Submenu 2</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to="/">Item 3</Link>
-          </li>
-        </ul>
-      </div>
-      <div class="navbar-end">
-        <Link to="/" class="btn">
-          Get started
-        </Link>
+      <div class="navbar-center hidden lg:flex navbar-end mr-20">
+        <ul class="menu menu-horizontal p-0">{menuItems}</ul>
       </div>
     </div>
   );
