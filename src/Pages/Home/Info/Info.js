cd7381ca54ff0f0d8d41abf7dbf7218d5aa924e3
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-10 18:45:17 +0600

    info card full style done

--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -8,20 +8,20 @@ const Info = () => {
   const info1 = {
     heading: "Opening Hours",
     paragraph: "Lorem Ipsum is simply dummy text of the pri",
-    gradient: true,
+    bgClass: "bg-gradient-to-r from-secondary to-primary",
   };
   const info2 = {
     heading: "Visit our location",
     paragraph: "Brooklyn, NY 10036, United States",
-    gradient: false,
+    bgClass: "bg-accent",
   };
   const info3 = {
     heading: "Contact us now",
     paragraph: "+000 123 456789",
-    gradient: true,
+    bgClass: "bg-gradient-to-r from-secondary to-primary",
   };
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 my-10">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 my-10 px-12">
       <InfoCards infoCard={info1} img={clock}></InfoCards>
       <InfoCards infoCard={info2} img={marker}></InfoCards>
       <InfoCards infoCard={info3} img={phone}></InfoCards>
--- src/Pages/Home/Info/InfoCard/InfoCards.js
+++ src/Pages/Home/Info/InfoCard/InfoCards.js
@@ -1,16 +1,11 @@
 import React from "react";
 
 const InfoCards = ({ infoCard, img }) => {
-  const { heading, paragraph, gradient } = infoCard;
-  console.log(gradient);
+  const { heading, paragraph, bgClass } = infoCard;
   return (
-    <div
-      className={`card lg:card-side ${
-        gradient ? "bg-gradient-to-r from-secondary to-primary" : "bg-accent"
-      } shadow-xl text-white p-5`}
-    >
+    <div className={`card lg:card-side shadow-xl text-white p-5 ${bgClass}`}>
       <figure>
-        <img src={img} alt="Album" />
+        <img className="p-0 lg:pl-5" src={img} alt="Album" />
       </figure>
       <div class="card-body">
         <h2 class="card-title">{heading}</h2>
