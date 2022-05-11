6275ab1d55abdae77cf860d11a2221775d88f288
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-11 10:41:56 +0600

    wibsite max width set

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navbar from "./Pages/Shared/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
