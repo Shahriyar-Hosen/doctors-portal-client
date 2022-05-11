c4e0fa421dbf12148fb8eb8a9b43a7e20a26c2fe
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-11 14:09:28 +0600

    Service data load

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,14 +1,25 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Service from "./Service";
 
 const AvailableAppointments = ({ date }) => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
 
   return (
     <div className="my-28">
       <h4 className="text-secondary text-xl text-center">
         Available Appointments on {format(date, "PP")}
       </h4>
-      
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
     </div>
   );
 };
