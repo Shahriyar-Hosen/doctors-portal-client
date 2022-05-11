c6c7f37c739772fd514151b606690089f652c69e
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-11 13:50:35 +0600

    setDate State update

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../Shared/Footer/Footer";
 import AppointmentBanner from "./AppointmentBanner";
 
 const Appointment = () => {
+  const [date, setDate] = useState(new Date());
   return (
     <div>
       <section className=" px-12">
-        <AppointmentBanner />
+        <AppointmentBanner date={date} setDate={setDate} />
       </section>
       <Footer></Footer>
     </div>
--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
+import React  from "react";
 import chair from "../../assets/images/chair.png";
 import bgChair from "../../assets/images/bg.png";
 import "react-day-picker/dist/style.css";
 import { DayPicker } from "react-day-picker";
 import { format } from "date-fns";
 
-const AppointmentBanner = () => {
-  const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({ date, setDate }) => {
   return (
     <div
       style={{ background: `url(${bgChair})`, backgroundSize: "cover" }}
       class="hero min-h-screen"
     >
       <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} class="max-w-sm  rounded-lg shadow-2xl" alt="" />
-        <div className="py-10 lg:px-10 ">
-          <div className="rounded-lg shadow-2xl bg-white">
-            <DayPicker mode="single" selected={date} onSelect={setDate} />
-          </div>
-          <p className="text-center">You Have Selected: {format(date, "PP")}</p>
+        <img
+          src={chair}
+          class="sm:max-w-sm md:max-w-md lg:max-w-lg rounded-lg shadow-2xl"
+          alt=""
+        />
+        <div>
+          <DayPicker mode="single" selected={date} onSelect={setDate} />
+          <p>You picked {format(date, "PP")}</p>
         </div>
       </div>
     </div>
