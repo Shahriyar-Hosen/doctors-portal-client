be968a9142cfa05316bf63aca8035aba2838d7d4
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-12 11:19:21 +0600

    signInWithGoogle done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,13 @@
 import React from "react";
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import auth from "../../firebase.init";
+// import auth from "../../firebase.init";
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user);
+    }
   return (
     <div className="flex h-screen justify-center items-center">
       <div class="card w-96 bg-base-100 shadow-xl">
@@ -8,7 +15,7 @@ const Login = () => {
           <h2 class="card-title text-2xl font-bold text-accent">Login</h2>
 
           <div class="divider">OR</div>
-          <button class="btn btn-outline btn-accent w-full">CONTINUE WITH GOOGLE</button>
+          <button onClick={() => signInWithGoogle()} class="btn btn-outline btn-accent w-full">CONTINUE WITH GOOGLE</button>
         </div>
       </div>
     </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyBaNgMMhPc4Nb6OkUpcqOCiQ1FG0u2uWZ4",
+  authDomain: "doctors-portal-c4226.firebaseapp.com",
+  projectId: "doctors-portal-c4226",
+  storageBucket: "doctors-portal-c4226.appspot.com",
+  messagingSenderId: "33337200597",
+  appId: "1:33337200597:web:3242590da2b8920caab75c",
 };
 
 // Initialize Firebase
