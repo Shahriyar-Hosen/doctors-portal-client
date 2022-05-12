bd4869178ec27248625ea36cdb05903bd03da747
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-12 10:53:52 +0600

    CONTINUE WITH GOOGLE btn add login form & react firebase hooks install

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login Page</h1>
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body items-center text-center">
+          <h2 class="card-title text-2xl font-bold text-accent">Login</h2>
+
+          <div class="divider">OR</div>
+          <button class="btn btn-outline btn-accent w-full">CONTINUE WITH GOOGLE</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
