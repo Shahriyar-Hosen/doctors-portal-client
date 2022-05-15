c6c3bafa1ba1c8f18a92865e01864d026f0545ff
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-15 14:28:26 +0600

    if status 401 or 403 navigate home & logout

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -1,18 +1,35 @@
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const MyAppointment = () => {
   const [user] = useAuthState(auth);
   const [appointments, setAppointments] = useState([]);
+  let navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
-      fetch(`http://localhost:5000/booking?patient=${user.email}`)
-        .then((res) => res.json())
-        .then((data) => setAppointments(data));
+      fetch(`http://localhost:5000/booking?patient=${user.email}`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => {
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem("accessToken");
+            navigate("/");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          return setAppointments(data);
+        });
     }
-  }, [user]);
+  }, [user, navigate]);
   return (
     <div>
       <div className="overflow-x-auto">
@@ -27,7 +44,7 @@ const MyAppointment = () => {
             </tr>
           </thead>
           <tbody>
-            {appointments.map((a, index) => (
+            {appointments?.map((a, index) => (
               <tr key={index}>
                 <th>{index + 1}</th>
                 <td>{a.patientName}</td>
