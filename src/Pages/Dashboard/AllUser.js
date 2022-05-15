915bdabf1e5316042809f5e45febd571515b70af
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-15 15:43:16 +0600

    All User map & show data table row done

--- src/Pages/Dashboard/AllUser.js
+++ src/Pages/Dashboard/AllUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useQuery } from "react-query";
 import Loading from "../Shared/Loading";
+import UserRow from "./UserRow";
 
 const AllUser = () => {
   const { isLoading, data: users } = useQuery(["users"], () =>
@@ -14,6 +15,23 @@ const AllUser = () => {
   return (
     <div>
       <h1 className="text-2xl">All User {users.length}</h1>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <UserRow key={user._id} user={user}></UserRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
