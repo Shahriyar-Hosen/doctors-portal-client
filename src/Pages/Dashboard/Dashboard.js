65f7d14fae922a41d0d171f836d84f764d55603f
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-15 19:58:05 +0600

    if is admin then show all user

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../firebase.init";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const [admin] = useAdmin(user);
   return (
     <div className="drawer drawer-mobile">
       <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
@@ -24,9 +29,11 @@ const Dashboard = () => {
           <li>
             <Link to="/dashboard/history">My History</Link>
           </li>
-          <li>
-            <Link to="/dashboard/users">All User</Link>
-          </li>
+          {admin && (
+            <li>
+              <Link to="/dashboard/users">All User</Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
