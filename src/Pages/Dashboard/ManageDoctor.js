d90e8eaee14cb5933cd02eea6229c9880dcb5aaa
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-17 14:36:23 +0600

    doctors data load

--- src/Pages/Dashboard/ManageDoctor.js
+++ src/Pages/Dashboard/ManageDoctor.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const ManageDoctor = () => {
-    return (
-        <div>
-            <h1>Manage Doctors</h1>
-        </div>
-    );
+  const { data: doctors, isLoading } = useQuery(["doctors"], () =>
+    fetch(`http://localhost:5000/doctors`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  return (
+    <div>
+      <h1>Manage Doctors: {doctors.length}</h1>
+    </div>
+  );
 };
 
 export default ManageDoctor;
